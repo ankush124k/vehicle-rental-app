8b2a138911de26749fdf48b7a4d1b1c3a5d0dc64
Ankush <ankush124k@gmail.com>
2025-09-05 22:13:31 +0530

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the Vehicle Rental App Backend!');
+});
+
+app.listen(PORT, (error) => {
+    if(error){
+        console.log(`server stopped ${error}`);
+        exit(1);
+        
+    }else{
+        console.log(`Server is running on http://localhost:${PORT}`);
+    }
+});
